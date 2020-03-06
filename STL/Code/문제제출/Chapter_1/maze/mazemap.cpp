558702751fea21a2db9234aa36fbf2aa381ae5e4
HyunjungLee-dev <hyunjunglee.dev@gmail.com>
2020-03-06 19:19:10 +0900

    Fix : unsigned int change size_type

--- STL/Code/문제제출/Chapter_1/maze/mazemap.cpp
+++ STL/Code/문제제출/Chapter_1/maze/mazemap.cpp
@@ -13,17 +13,17 @@ void main()
 	file >> width >> height;
 	
 		vector<vector<int>> maze(height, vector<int>(width, 0));
-		for (unsigned int i = 0; i < maze.size(); i++)
+		for (vector<int>::size_type i = 0; i < maze.size(); i++)
 		{
-			for (unsigned int j = 0; j < maze[i].size(); j++)
+			for (vector<int>::size_type j = 0; j < maze[i].size(); j++)
 			{
 				file >> temp;
 				maze[i][j] = temp;
 			}
 		}
 
-		for (unsigned int i = 0; i < maze.size(); i++) {
-			for (unsigned int j = 0; j < maze[i].size(); j++) {
+		for (vector<int>::size_type i = 0; i < maze.size(); i++) {
+			for (vector<int>::size_type j = 0; j < maze[i].size(); j++) {
 				if (maze[i][j] == 0)
 				{
 					cout << "¢Ë";
